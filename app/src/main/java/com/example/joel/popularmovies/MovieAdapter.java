9f0e43fb2158909a74c532e84ae17bcebfea563e
jfranc01 <goofgog555@yahoo.com>
2016-12-14 15:37:37 -0500

    Phase1

--- app/src/main/java/com/example/joel/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/joel/popularmovies/MovieAdapter.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  * Created by joel on 2016-12-13.
- * Test to see if this change will be committed
+ * Test to see if this change will be committed again
  */
 
 public class MovieAdapter extends ArrayAdapter<Movie> {
